a115c8e85e37f32c9f9f718b7ad82c9b74885145
Dennis <d.weggenmann@web.de>
2022-04-18 14:06:12 +0200

    rename test file

--- src/testMergeIntervals.py
+++
@@ -1,8 +0,0 @@
-from mergeIntervals import mergeOverlappingIntervals
-arr = [[25, 30], [2, 19], [14, 23], [4, 8]]
-mergedarr = [[2,23] ,[25,30]]
-
-assert mergeOverlappingIntervals(arr) == mergedarr
-
-
-
