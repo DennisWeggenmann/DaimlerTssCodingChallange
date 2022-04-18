91a8e76e3517b398338abbfd06a6588f1b9e2171
Dennis <d.weggenmann@web.de>
2022-04-18 14:15:15 +0200

    add function so pytest will run on github actions

--- src/test_mergeIntervals.py
+++ src/test_mergeIntervals.py
@@ -1,9 +1,8 @@
-
-from mergeIntervals import mergeOverlappingIntervals
-arr = [[25, 30], [2, 19], [14, 23], [4, 8]]
-mergedarr = [[2,23] ,[25,30]]
-
-assert mergeOverlappingIntervals(arr) == mergedarr
+def test_mergeIntervals():
+    from mergeIntervals import mergeOverlappingIntervals
+    arr = [[25, 30], [2, 19], [14, 23], [4, 8]]
+    mergedarr = [[2,23] ,[25,30]]
+    assert mergeOverlappingIntervals(arr) == mergedarr
 
 
 
