8f84c424dc385a6f645f0a707e47e790684ebc97
Dennis <d.weggenmann@web.de>
2022-04-18 15:09:34 +0200

    add tests

--- src/test_mergeIntervals.py
+++ src/test_mergeIntervals.py
@@ -1,12 +1,22 @@
 #test if mergeIntervals works
 #Author: Dennis Weggenmann
-arr = [[25, 30], [2, 19], [14, 23], [4, 8]]
-mergedarr = [[2,23] ,[25,30]]
+from mergeIntervals import mergeOverlappingIntervals
+overlap = [[25, 30], [2, 19], [14, 23], [4, 8]]
+overlapResult = [[2,23] ,[25,30]]
+
+noOverlap = [[1,2],[3,4]]
+noOverlapResult = [[1,2],[3,4]]
+def test_mergeIntervals_noOverlap():
+    assert mergeOverlappingIntervals(noOverlap) == noOverlapResult
 
 def test_mergeIntervals():
-    from mergeIntervals import mergeOverlappingIntervals
-    
-    assert mergeOverlappingIntervals(arr) == mergedarr
+    assert mergeOverlappingIntervals(overlap) == overlapResult
+
+def test_mergeIntervals_empty():
+    assert mergeOverlappingIntervals([]) == []
+
+def test_mergeIntervals_one():
+    assert mergeOverlappingIntervals([[1,2]]) == [[1,2]]
 
 
 
